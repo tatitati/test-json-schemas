463861bcce3c9f2b489478017f71770024555618
tatitati <francis.jaa@protonmail.com>
2022-11-08 10:31:53 +0100

    more

---
+++ TestReferenceCustomProperties.py
@@ -0,0 +1,69 @@
+from jsonschema import validate
+
+class TestReferenceCustomProperties():
+
+	def test_referenced_custom_properties(self):
+		schema = {
+			"definitions": {},
+			"$schema": "http://json-schema.org/draft-07/schema#",
+			"$id": "https://example.com/345345345.json",
+			"title": "Root",
+			"type": "object",
+			"required": [
+				"name",
+				"capital"
+			],
+			"properties": {
+				"name": {
+					"$id": "#root/name",
+					"title": "Name",
+					"type": "string",
+					"default": "",
+					"examples": [
+						"Eggs"
+					],
+					"pattern": "^.*$"
+				},
+				"capital": {
+					"$id": "#root/capital",
+					"type": "object",
+					"$ref": "#/$defs/capital"
+				}
+			},
+			"$defs": {
+				"capital": {
+					"type": "object",
+					"properties": {
+						"name": {
+							"$id": "#root/capital/name",
+							"title": "Name",
+							"type": "string",
+							"default": "",
+							"examples": [
+								"Eggs"
+							],
+							"pattern": "^.*$"
+						},
+						"population": {
+							"$id": "#root/capital/population",
+							"title": "population",
+							"type": "number",
+							"examples": [
+								34.99
+							],
+							"default": 0.0
+						}
+					}
+				}
+			  }
+		}
+
+		data = {
+			"name" : "Eggs",
+			"capital" : {
+				"name": "madrid",
+				"population": 34
+			}
+		}
+
+		validate(instance=data, schema=schema)
