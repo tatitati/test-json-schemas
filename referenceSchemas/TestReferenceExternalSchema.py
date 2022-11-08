216b42404bc32e9be5bf6f15a9586c3ac01145f6
tatitati <francis.jaa@protonmail.com>
2022-11-08 10:20:31 +0100

    files

---
+++ referenceSchemas/TestReferenceExternalSchema.py
@@ -0,0 +1,41 @@
+from jsonschema import validate
+
+class TestReferenceExternalSchema():
+	def test_referenced_schema(self):
+		schema = {
+			"definitions": {},
+			"$schema": "http://json-schema.org/draft-07/schema#",
+			"$id": "https://example.com/country.json",
+			"title": "Root",
+			"type": "object",
+			"required": [
+				"name",
+				"capital"
+			],
+			"properties": {
+				"name": {
+					"$id": "#root/name",
+					"title": "Name",
+					"type": "string",
+					"default": "",
+					"examples": [
+						"Eggs"
+					],
+					"pattern": "^.*$"
+				},
+				"capital": {
+					"type": "object",
+					"$ref": "file:city.json"
+				}
+			}
+		}
+
+		data = {
+			"name" : "Eggs",
+			"capital" : {
+				"name": "madrid",
+				"population": 34.4
+			}
+		}
+
+		validate(instance=data, schema=schema)
